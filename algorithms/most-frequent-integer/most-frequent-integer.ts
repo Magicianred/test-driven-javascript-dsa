06439c1d8060d809922e44424653ba41cae62910
pjnalls <preston.james.nalls@gmail.com>
2021-01-03 06:41:33 -0600

    Begin TDD for algorithm.

---
+++ algorithms/most-frequent-integer/most-frequent-integer.ts
@@ -0,0 +1,35 @@
+/*
+ *
+ *  Find the most frequent integer in a given array of numbers.
+ *
+ */
+
+const testCases : Array<Array<number>> = [
+  // Case 1: [1, 2, 2, 4, 0.5, 0.5, 0.5, 3] => 2
+  [1, 2, 2, 4, 0.5, 0.5, 0.5, 3, 2],
+  
+  // Case 2: [3, 5, 7] => 3
+  [3, 5, 7, 3], 
+
+  // Case 3: [4, 5, 89, 5, 1] => 5
+  [4, 5, 89, 5, 1, 5],
+
+  // Case 4: [1] => ?
+  [1],
+
+  // Case 5: [-99, -99, -99, 1, -17, 0, 1, -99] => -99
+  [-99, -99, -99, 1, -17, 0, 1, -99]
+];
+
+function getMostFrequentIntegerOf(integers : Array<number>) : number {
+  return 0;
+}
+
+testCases.forEach((testCase : Array<number>) => {
+  const
+    expectedOutput : number = testCase.pop(),
+    actualOutput : number = getMostFrequentIntegerOf(testCase);
+  expectedOutput === actualOutput ?
+    console.log('\x1b[32m', actualOutput) :
+    console.log('\x1b[31m', `${actualOutput}, but expected ${expectedOutput}.`);
+});
