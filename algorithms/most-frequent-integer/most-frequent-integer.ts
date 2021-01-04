df9c3f49d8102f208c8ba2cb8649008f4d820007
pjnalls <preston.james.nalls@gmail.com>
2021-01-04 04:01:15 -0600

    Add more test cases & implement function.

--- algorithms/most-frequent-integer/most-frequent-integer.ts
+++ algorithms/most-frequent-integer/most-frequent-integer.ts
@@ -4,7 +4,6 @@
  *
  */
 
- // TODO: Cover other edge cases.
 const testCases : Array<Array<number>> = [
   // Case 1: [1, 2, 2, 4, 0.5, 0.5, 0.5, 3] => 2
   [1, 2, 2, 4, 0.5, 0.5, 0.5, 3, 2],
@@ -15,19 +14,39 @@ const testCases : Array<Array<number>> = [
   // Case 3: [4, 5, 89, 5, 1] => 5
   [4, 5, 89, 5, 1, 5],
 
-  // Case 4: [1] => ?
-  [1],
+  // Case 4: [-99, -99, -99, 1, -17, 0, 1, -99] => -99
+  [-99, -99, -99, 1, -17, 0, 1, -99],
 
-  // Case 5: [-99, -99, -99, 1, -17, 0, 1, -99] => -99
-  [-99, -99, -99, 1, -17, 0, 1, -99]
+  // Case 5: [Number.MAX_VALUE, Number.MAX_VALUE, 1] => Number.MAX_VALUE
+  [Number.MAX_VALUE, Number.MAX_VALUE, 1, Number.MAX_VALUE],
+
+  // Case 6: [Number.MIN_VALUE, Number.MIN_VALUE, 1] => 1
+  [Number.MIN_VALUE, Number.MIN_VALUE, 1, 1],
+
+  // Case 7: [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, 0] => Number.POSITIVE_INFINITY
+  [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, 0, Number.POSITIVE_INFINITY],
+
+  // Case 8: [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, 0] => Number.NEGATIVE_INFINITY
+  [Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, 0, Number.NEGATIVE_INFINITY],
+
+  // Case 9: [1, 1] => 1
+  [1, 1]
 ];
 
-// TODO: Implement.
 function getMostFrequentIntegerOf(integers : Array<number>) : number {
-  return 0;
+  const frequencies : Object = {};
+  let mostFrequentInteger : number = 0;
+
+  integers.forEach((x : number) : void => { 
+    x % 1 === 0 && frequencies[`${x}`] ? frequencies[`${x}`]++ : frequencies[`${x}`] = 1;
+    if (frequencies[`${x}`] > (frequencies[`${mostFrequentInteger}`] ?? 0))
+      mostFrequentInteger = x;
+  });
+
+  return mostFrequentInteger;
 }
 
-testCases.forEach((testCase : Array<number>) => {
+testCases.forEach((testCase : Array<number>): void => {
   const
     expectedOutput : number = testCase.pop(),
     actualOutput : number = getMostFrequentIntegerOf(testCase);
