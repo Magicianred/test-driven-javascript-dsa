501f4503e0f853688d7c5d0b9f8bd94d5bfa1309
pjnalls <preston.james.nalls@gmail.com>
2021-02-07 08:53:41 -0600

    Enhance output for "runTestsTo..." method

--- utilities/testing-utils.js
+++ utilities/testing-utils.js
@@ -1,10 +1,22 @@
 exports.runTestsTo = (solution, testCases) => {
+  let failureCount = 0;
+
+  // Add line break for console output.
+  console.log();
+
   testCases.forEach(testCase => {
     const
       expectedOutput = testCase.pop(),
       actualOutput = solution(testCase);
     expectedOutput === actualOutput ?
-      console.log('\x1b[32m', `${actualOutput}`) :
-      console.log('\x1b[31m', `${actualOutput}, but expected ${expectedOutput}.`);
+      console.log('\x1b[32m', `✓ Output is ${actualOutput} as expected.`) :
+      (failureCount++, console.log('\x1b[31m', `✖ Output is ${actualOutput} but expected ${expectedOutput}.`));
   });
+
+  // Add line break for console output.
+  console.log();
+  
+  failureCount < 1 ? 
+    console.log('\x1b[32m', `✓ All tests for '${solution.name}' solution passed.`) : 
+    console.log('\x1b[31m', `✖ ${failureCount} of ${testCases.length - 1} tests for '${solution.name}' solution failed.`)
 }
