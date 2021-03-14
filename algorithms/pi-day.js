111411b6ede995ff60bf913391ceb7e7f2cfb7f5
Preston <preston.james.nalls@gmail.com>
2021-03-14 16:54:01 -0500

    Compute Math.PI for Pi Day.

---
+++ algorithms/pi-day.js
@@ -0,0 +1,18 @@
+/*
+ *
+ *  Efficiently calculate Math.PI for Pi Day.
+ * 
+ */
+
+'use strict';
+
+const testingUtils = require('../utilities/testing-utils');
+
+const testCases = [
+  // Case Math.PI: [undefined, Math.PI], undefined => Math.PI
+  [undefined, Math.PI]
+];
+
+const pi = () => Math.floor((16 * Math.atan(1 / 5) - (4 * Math.atan(1 / 239))) * (10 ** 15)) / (10 ** 15);
+
+testingUtils.runTestsTo(pi, testCases)
