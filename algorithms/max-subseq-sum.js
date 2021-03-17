e297137286caa7ebad3b69408fd27d4d18ad6325
pjnalls <preston.james.nalls@gmail.com>
2021-03-17 15:25:13 -0500

    Use Kadane's algorithm to implement solution.

--- algorithms/max-subseq-sum.js
+++ algorithms/max-subseq-sum.js
@@ -1,19 +1,26 @@
 /*
  *
- *  Give (possibly negative) integers, find the maximum sum of a subsequence.
+ *
+ *  Given (possibly negative) integers, find the maximum sum of a subsequence
+ *  with Kadane's algorithm.
  * 
+ *  Note: return 0 if all given integers are negative.
+ *
  */
 
-'use strict';
+"use strict";
 
-const testingUtils = require('../utilities/testing-utils');
+const testingUtils = require("../utilities/testing-utils");
 
 const testCases = [
+  // Case Z: [2, 11, -4, 13, -5, -2] => 22
+  [2, 11, -4, 13, -5, -2, 22],
+
   // Case 0 (Average): [-2, 11, -4, 13, -5, -2] => 20
   [-2, 11, -4, 13, -5, -2, 20],
 
-  // Case 1: [-1, -2, -3, -2, -1] => -3
-  [-1, -2, -3, -2, -1, -3],
+  // Case 1: [-1, -2, -3, -2, -1] => 0
+  [-1, -2, -3, -2, -1, 0],
 
   // Case 2: [1, 2, 3, 2, 1] => 9
   [1, 2, 3, 2, 1, 9],
@@ -21,12 +28,12 @@ const testCases = [
   // Case 3: [1, 2, 3, -2, 1] => 6
   [1, 2, 3, -2, 1, 6],
 
-  // Case 4: [-1, -2, 3, -2, -1] => 1
-  [-1, -2, 3, -2, -1, 1],
+  // Case 4: [-1, -2, 3, -2, -1] => 3
+  [-1, -2, 3, -2, -1, 3],
 
-  // Case 5: [-3, -2, -2, -1, -1] => -2
-  [-3, -2, -2, -1, -1, -2],
-  
+  // Case 5: [-3, -2, -2, -1, -1] => 0
+  [-3, -2, -2, -1, -1, 0],
+
   // Case 6: [-2, 4, -1, 1, 1] => 5
   [-2, 4, -1, 1, 1, 5],
 
@@ -40,10 +47,40 @@ const testCases = [
   [1, 2, 3, -1, -2, 2, 2, -10, 7],
 
   // Case 10: [1, 2, 3, -1, -2, 1, -10, 16] => 7
-  [1, 2, 3, -1, -2, 1, -10, 16, 7]
+  [1, 2, 3, -1, -2, 1, -10, 16, 16],
+
+  // Case 11 (Explicitly Undefined): [] => undefined
+  [undefined],
+
+  // Case 12: [1] => 1
+  [1, 1],
+
+  // Case 13: [-1, -1] => 0
+  [-1, -1, 0],
+
+  // Case 14: [-1, 2, -1] => 2
+  [-1, 2, -1, 2],
+
+  // Case 15 (Implicitly Undefined): [] => undefined
+  [],
+
+  // Case 16: 1 => undefined
+  1,
 ];
 
-// TODO: Provide implementation for stubbed solution.
-const findMaxSubsequenceSumOf = integers => 0;
+const findMaxSubsequenceSumOf = (integers) => {
+  // sums
+  let best = 0,
+    current = 0;
 
-testingUtils.runTestsTo(findMaxSubsequenceSumOf, testCases)
+  if (!Array.isArray(integers) || integers.length <= 0) return undefined;
+  else
+    integers.forEach((x) => {
+      current = Math.max(0, current + x);
+      best = Math.max(best, current);
+    });
+
+  return best;
+};
+
+testingUtils.runTestsTo(findMaxSubsequenceSumOf, testCases);
