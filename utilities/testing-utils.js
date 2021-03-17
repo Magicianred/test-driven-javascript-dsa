9ba56e2624ece8ee8fe09f9a2c71ff07123e3912
pjnalls <preston.james.nalls@gmail.com>
2021-03-17 13:11:09 -0500

    Add type-checker and prettify the code.

--- utilities/testing-utils.js
+++ utilities/testing-utils.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 exports.runTestsTo = (solution, testCases) => {
   let failureCount = 0;
@@ -6,22 +6,39 @@ exports.runTestsTo = (solution, testCases) => {
   // Add line break to console output.
   console.log();
 
-  testCases.forEach(testCase => {
-    const
-      expectedOutput = testCase.pop(),
-      actualOutput = solution(testCase);
-    expectedOutput === actualOutput ?
-      console.log('\x1b[32m', `✓ Output is ${actualOutput} as expected.`) :
-      (failureCount++, console.log('\x1b[31m', `✖ Output is ${actualOutput} but expected ${expectedOutput}.`));
-  });
+  if (Array.isArray(testCases)) {
+    testCases.forEach((testCase) => {
+      const expectedOutput =
+          Array.isArray(testCase) && testCase.length > 0
+            ? testCase.pop()
+            : undefined,
+        actualOutput = solution(testCase);
+
+      expectedOutput === actualOutput
+        ? console.log("\x1b[32m", `✓ Output is ${actualOutput} as expected.`)
+        : (failureCount++,
+          console.log(
+            "\x1b[31m",
+            `✖ Output is ${actualOutput} but expected ${expectedOutput}.`
+          ));
+    });
+  } else console.log("\x1b[31m", `✖ Test Cases aren't of type 'Array'.`);
 
   // Add line break to console output.
   console.log();
-  
-  failureCount <= 0 ? 
-    console.log('\x1b[32m', `✓ All tests for '${solution.name}' solution passed.`) :
-  
-  failureCount === testCases.length ?
-    console.log('\x1b[31m', `✖ All tests for '${solution.name}' solution failed.`) : 
-    console.log('\x1b[31m', `✖ ${failureCount} of ${testCases.length} tests for '${solution.name}' solution failed.`)
-}
+
+  failureCount <= 0
+    ? console.log(
+        "\x1b[32m",
+        `✓ All tests for '${solution.name}' solution passed.`
+      )
+    : failureCount === testCases.length
+    ? console.log(
+        "\x1b[31m",
+        `✖ All tests for '${solution.name}' solution failed.`
+      )
+    : console.log(
+        "\x1b[31m",
+        `✖ ${failureCount} of ${testCases.length} tests for '${solution.name}' solution failed.`
+      );
+};
