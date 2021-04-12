8bb10f41deff03e103600e05674120c3b44100a7
Preston <preston.james.nalls@gmail.com>
2021-04-12 07:19:18 -0500

    Add Tree methods & start implementing 'printTree'.

--- data-structures/trees/binary-search-tree.js
+++ data-structures/trees/binary-search-tree.js
@@ -1,10 +1,51 @@
 const testingUtils = require("../../utilities/testing-utils");
 const testCases = [
-  [1, 3, 21, 8, 1, 13, 5, 34, "1, 1, 3, 5, 8, 13, 21, 34"],
-  [1, "1"],
-  ["b", "f", "e", "d", "g", "c", "h", "a", "a, b, c, d, e, f, g, h"],
-  ["3", "2", "a", "1", "b", "1, 2, 3, a, b"],
-  [3, 2, undefined, "2, 3"],
+  [1, 3, 21, 8, 1, 13, 5, 34, "in-order", "1, 1, 3, 5, 8, 13, 21, 34"],
+  [1, 3, 21, 8, 1, 13, 5, 34, "pre", "1, 1, 3, 5, 8, 13, 21, 34"],
+  [1, 3, 21, 8, 1, 13, 5, 34, "post", "1, 1, 3, 5, 8, 13, 21, 34"],
+  [
+    1,
+    3,
+    21,
+    8,
+    1,
+    13,
+    5,
+    34,
+    "tree",
+    `
+         8
+        / \\
+       3   34
+      / \\ / \\
+     1  5  13 34
+    /
+   1
+`,
+  ],
+  [1, "in-order", "1"],
+  [1, "pre", "1"],
+  [1, "post", "1"],
+  [
+    "b",
+    "f",
+    "e",
+    "d",
+    "g",
+    "c",
+    "h",
+    "a",
+    "in-order",
+    "a, b, c, d, e, f, g, h",
+  ],
+  ["b", "f", "e", "d", "g", "c", "h", "a", "pre", "a, b, c, d, e, f, g, h"],
+  ["b", "f", "e", "d", "g", "c", "h", "a", "post", "a, b, c, d, e, f, g, h"],
+  ["3", "2", "a", "1", "b", "in-order", "1, 2, 3, a, b"],
+  ["3", "2", "a", "1", "b", "pre", "1, 2, 3, a, b"],
+  ["3", "2", "a", "1", "b", "post", "1, 2, 3, a, b"],
+  [3, 2, undefined, "in-order", "2, 3"],
+  [3, 2, undefined, "pre", "2, 3"],
+  [3, 2, undefined, "post", "2, 3"],
 ];
 
 const Tree = function () {
@@ -12,48 +53,122 @@ const Tree = function () {
   this.stringBuilder = "";
 };
 
-Tree.prototype.print = function () {
-  for(const data of this.root.visit())
-    this.stringBuilder += data;
+Tree.prototype.print = function (order, length = null) {
+  if (order === "pre")
+    for (const data of this.root.preOrderTraversal(this.root))
+      this.stringBuilder += data;
+  else if (order === "post")
+    for (const data of this.root.postOrderTraversal(this.root))
+      this.stringBuilder += data;
+  else if (order === "in-order")
+    for (const data of this.root.inOrderTraversal(this.root))
+      this.stringBuilder += data;
+  else {
+    this.stringBuilder += "\n";
+
+    for (let i = 0; i < length; i++) this.stringBuilder += " ";
+    this.stringBuilder += `${this.root.data}`;
+    for (let i = 0; i < length; i++) this.stringBuilder += " ";
+
+    for (const data of this.root.printTree(this.root, length, 1))
+      this.stringBuilder += data;
+    
+    return this.stringBuilder;
+  }
+
   return this.stringBuilder.trim().split(" ").join(", ");
 };
 
 Tree.prototype.insert = function (data) {
-  const node = new BinarySearchTreeNode(data);
+  const node = new Node(data);
   this.root === null ? (this.root = node) : this.root.insert(node);
 };
 
-const BinarySearchTreeNode = function (data, left = null, right = null) {
+const Node = function (data, left = null, right = null) {
   this.data = data;
   this.left = left;
   this.right = right;
 };
 
-BinarySearchTreeNode.prototype.insert = function (node) {
-  if (node.data <= this.data) {
+Node.prototype.insert = function (node) {
+  if (node.data <= this.data)
     this.left === null ? (this.left = node) : this.left.insert(node);
-  } else if (node.data > this.data) {
-    this.right === null ? (this.right = node) : this.right.insert(node);
+  else this.right === null ? (this.right = node) : this.right.insert(node);
+};
+
+Node.prototype.inOrderTraversal = function* (node) {
+  if (node !== null) {
+    yield* this.inOrderTraversal(node.left);
+    yield `${node.data} `;
+    yield* this.inOrderTraversal(node.right);
   }
 };
 
-BinarySearchTreeNode.prototype.visit = function* () {
-  // Go left.
-  if (this.left !== null) yield* this.left.visit();
-  // Get node data.
-  yield `${this.data} `;
-  // Go right.
-  if (this.right !== null) yield* this.right.visit();
+Node.prototype.preOrderTraversal = function* (node) {
+  if (node !== null) {
+    yield `${node.data} `;
+    yield* this.preOrderTraversal(node.left);
+    yield* this.preOrderTraversal(node.right);
+  }
 };
 
-const createBinarySearchTreeWith = (testCase) => {
-  const bst = new Tree();
-  testCase.forEach((element) => {
-    bst.insert(element);
-  });
-  return bst.print();
+Node.prototype.postOrderTraversal = function* (node) {
+  if (node !== null) {
+    yield* this.postOrderTraversal(node.left);
+    yield* this.postOrderTraversal(node.right);
+    yield `${node.data} `;
+  }
+};
+Node.prototype.breadthFirstTraversal = function* (node, length, level) {
+  if (node === null) return;
+  if (level == 1) {
+    let stringBuilder = "\n";
+    for (let i = 0; i < length; i++) stringBuilder += " ";
+    stringBuilder += "/ \\\n";
+    for (
+      let i = 0;
+      i < length - `${node.left ? node.left.data : "null"}`.length - 1;
+      i++
+    )
+      stringBuilder += "  ";
+    stringBuilder += `${node.left ? node.left.data : "null"}   ${
+      node.right ? node.right.data : "null"
+    }`;
+    yield stringBuilder;
+  } else if (level > 1) {
+    yield* this.breadthFirstTraversal(node.root.left, level - 1);
+    yield* this.breadthFirstTraversal(node.right, level - 1);
+  }
+};
+Node.prototype.printTree = function* (node, length, level) {
+  if (this.root == null) return;
+  if (level == 1) {
+    let stringBuilder = "\n";
+    for (let i = 0; i < length; i++) stringBuilder += " ";
+    stringBuilder += "/ \\\n";
+    for (
+      let i = 0;
+      i < length - `${node.left ? node.left.data : "null"}`.length - 1;
+      i++
+    )
+      stringBuilder += "  ";
+    stringBuilder += `${node.left ? node.left.data : "null"}   ${
+      node.right ? node.right.data : "null"
+    }`;
+    yield stringBuilder;
+  } else if (level > 1) {
+    yield* this.printTree(node.left, length - 1, level - 1);
+    yield* this.printTree(node.right, length - 1, level - 1);
+  }
 };
 
-testingUtils.runTestsTo(createBinarySearchTreeWith, testCases);
+const printBST = (testCase) => {
+  const bst = new Tree(),
+    order = testCase.pop();
+  testCase.forEach((element) => bst.insert(element));
+  return bst.print(order, testCase.length);
+};
 
-module.exports = exports = { BinarySearchTreeNode, Tree };
+testingUtils.runTestsTo(printBST, testCases);
+
+module.exports = exports = { Tree, Node };
